46fb36ed028c4847c6402ffbfced50c4a1e7ebf1
bigboss <damian@odiseo.com.ar>
2021-03-17 12:25:55 -0300

    fixes

--- src/Controller/VendorSlugController.php
+++ src/Controller/VendorSlugController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Odiseo\SyliusVendorPlugin\Controller;
 
-use Sylius\Component\Product\Generator\SlugGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,15 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class VendorSlugController extends AbstractController
 {
-    /** @var SlugGeneratorInterface */
-    private $slugGenerator;
-
-    public function __construct(
-        SlugGeneratorInterface $slugGenerator
-    ) {
-        $this->slugGenerator = $slugGenerator;
-    }
-
     /**
      * @param Request $request
      * @return Response
@@ -31,7 +21,7 @@ final class VendorSlugController extends AbstractController
         $name = $request->query->get('name');
 
         return new JsonResponse([
-            'slug' => $this->slugGenerator->generate($name),
+            'slug' => $this->get('sylius.generator.slug')->generate($name),
         ]);
     }
 }
