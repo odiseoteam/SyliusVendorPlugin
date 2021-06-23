fe01af900d6817b1604cf609b60afdaec2a46eeb
bigboss <damian@odiseo.com.ar>
2021-06-23 14:03:36 -0300

    fixes

--- src/Controller/VendorController.php
+++ src/Controller/VendorController.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Odiseo\SyliusVendorPlugin\Controller;
 
 use Odiseo\SyliusVendorPlugin\Entity\VendorInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Sylius\Component\Resource\ResourceActions;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Webmozart\Assert\Assert;
 
 class VendorController extends ResourceController
 {
@@ -18,33 +20,71 @@ class VendorController extends ResourceController
      * @param Request $request
      * @return Response
      */
-    public function updatePositionsAction(Request $request): Response
+    public function updateVendorPositionsAction(Request $request): Response
     {
         $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
         $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
-        $vendorsToUpdate = $request->get('vendors');
+        $vendors = $request->get('vendors');
 
-        if ($configuration->isCsrfProtectionEnabled() && !$this->isCsrfTokenValid('update-vendor-position', $request->request->get('_csrf_token'))) {
+        $this->validateCsrfProtection($request, $configuration);
+
+        if ($this->shouldProductsPositionsBeUpdated($request, $vendors)) {
+            /** @var Session $session */
+            $session = $request->getSession();
+
+            /**
+             * @var int $id
+             * @var string $position
+             */
+            foreach ($vendors as $id => $position) {
+                try {
+                    $this->updatePositions($position, $id);
+                } catch (\InvalidArgumentException $exception) {
+                    $session->getFlashBag()->add('error', $exception->getMessage());
+
+                    return $this->redirectHandler->redirectToReferer($configuration);
+                }
+            }
+
+            $this->manager->flush();
+        }
+
+        return $this->redirectHandler->redirectToReferer($configuration);
+    }
+
+    /**
+     * @param Request $request
+     * @param RequestConfiguration $configuration
+     */
+    private function validateCsrfProtection(Request $request, RequestConfiguration $configuration): void
+    {
+        /** @var string|null $token */
+        $token = $request->request->get('_csrf_token');
+        if ($configuration->isCsrfProtectionEnabled() && !$this->isCsrfTokenValid('update-vendor-position', $token)) {
             throw new HttpException(Response::HTTP_FORBIDDEN, 'Invalid csrf token.');
         }
+    }
 
-        if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true) && null !== $vendorsToUpdate) {
-            /** @var array $vendorToUpdate */
-            foreach ($vendorsToUpdate as $vendorToUpdate) {
-                if (!is_numeric($vendorToUpdate['position'])) {
-                    throw new HttpException(
-                        Response::HTTP_NOT_ACCEPTABLE,
-                        sprintf('The vendor position "%s" is invalid.', $vendorToUpdate['position'])
-                    );
-                }
+    /**
+     * @param Request $request
+     * @param array|null $vendors
+     * @return bool
+     */
+    private function shouldProductsPositionsBeUpdated(Request $request, ?array $vendors): bool
+    {
+        return in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true) && null !== $vendors;
+    }
 
-                /** @var VendorInterface $vendor */
-                $vendor = $this->repository->findOneBy(['id' => $vendorToUpdate['id']]);
-                $vendor->setPosition((int) $vendorToUpdate['position']);
-                $this->manager->flush();
-            }
-        }
+    /**
+     * @param string $position
+     * @param int $id
+     */
+    private function updatePositions(string $position, int $id): void
+    {
+        Assert::numeric($position, sprintf('The position "%s" is invalid.', $position));
 
-        return new JsonResponse();
+        /** @var VendorInterface $vendorFromBase */
+        $vendorFromBase = $this->repository->findOneBy(['id' => $id]);
+        $vendorFromBase->setPosition((int) $position);
     }
 }
